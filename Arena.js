822efaf3020bfd7011b3682b4cb7389dde1b72bd
tpoliveira-mariana <mariana.tpdeoliveira@gmail.com>
2019-10-12 16:27:24 +0100

    Changed walls' collor

--- Arena.js
+++ Arena.js
@@ -2,6 +2,7 @@ class Arena extends SceneObject {
     constructor(x, y, z) {
         super()
 
+        let wallMAT = new THREE.MeshBasicMaterial({wireframe: true, color: 0x9fd4d4})
         let baseMAT = new THREE.MeshBasicMaterial({wireframe: true, color: 0x6e7574})
         let boxThickness = 1
         let boxWidth = 80
@@ -13,9 +14,9 @@ class Arena extends SceneObject {
         this.add(bottom)
 
         // add arena's walls
-        let wall1 = super.createSceneObjBox(0.5, -29.5, 5.5, boxWidth - 1, boxThickness, boxHeight, baseMAT.clone())
-        let wall2 = super.createSceneObjBox(0.5, 29.5, 5.5, boxWidth - 1, boxThickness, boxHeight, baseMAT.clone())
-        let wall3 = super.createSceneObjBox(-39.5, 0, 5.5, boxThickness, boxDepth, boxHeight, baseMAT.clone())
+        let wall1 = super.createSceneObjBox(0.5, -29.5, 5.5, boxWidth - 1, boxThickness, boxHeight, wallMAT.clone())
+        let wall2 = super.createSceneObjBox(0.5, 29.5, 5.5, boxWidth - 1, boxThickness, boxHeight, wallMAT.clone())
+        let wall3 = super.createSceneObjBox(-39.5, 0, 5.5, boxThickness, boxDepth, boxHeight, wallMAT.clone())
 
         bottom.add(wall1)
         bottom.add(wall2)
