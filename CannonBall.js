f3f2543b04e1f4877a833b8fb556504c681d067b
joanaalvoeiro <joanaalvoeiro@gmail.com>
2019-10-13 20:41:28 +0100

    Balls rotate on x axis, changed ball axesHelper position

--- CannonBall.js
+++ CannonBall.js
@@ -3,7 +3,8 @@ class CannonBall extends SceneObject {
         super()
         
         let MAT = new THREE.MeshBasicMaterial({wireframe: true, color: 0x0})
-        let ball = super.createSceneObjSphere(-5, 0, 0, 1, 20, 20, 0, Math.PI * 2, MAT)
+        let radius = 2
+        let ball = super.createSceneObjSphere(-5, 0, 0, radius, 20, 20, 0, Math.PI * 2, MAT)
         this.add(ball)
 
         this.position.set(pos.x, pos.y,pos.z)
@@ -13,12 +14,12 @@ class CannonBall extends SceneObject {
         let init_velocity = Math.random() * 5 + 1
         this.speed = new THREE.Vector3(init_velocity, init_velocity, 0);
 
-        this.add(new THREE.AxesHelper(1))
+        let axis = new THREE.AxesHelper(2)
+        axis.position.set(-2*radius, 0, radius)
+        this.add(axis)
 
         
-        this.velocity = new THREE.Vector3(init_velocity, init_velocity, 0);
-
-
+        this.velocity = new THREE.Vector3(init_velocity/radius, init_velocity/radius, 0);
     }
 
     // so far only moves horizontally
@@ -29,6 +30,7 @@ class CannonBall extends SceneObject {
         let elasticity = 0.8
 
         this.translateX(-this.speed.x * delta_time)
+        this.rotateX(-this.velocity.y * delta_time)
         /*this.translateY(this.speed.y * delta_time)
         this.translateZ(this.speed.z * delta_time)
         */
@@ -42,6 +44,9 @@ class CannonBall extends SceneObject {
         this.speed.x *= friction
         this.speed.y *= friction
         this.speed.z *= friction
+        this.velocity.x *= friction
+        this.velocity.y *= friction
+        this.velocity.z *= friction
 
         // Check if ball hit Arena's walls
         if (this.position.x <= -34) {
--- Scene.js
+++ Scene.js
@@ -78,7 +78,7 @@ function createCameras() {
     'use strict'
 
     camera_top = new Camera(15, 0, 20, new THREE.Vector3(15, 0, 0 ))
-    camera_persp = new PerspCamera(100, -40, 50, new THREE.Vector3(0, 0, 0))
+    camera_persp = new PerspCamera(90, -40, 50, new THREE.Vector3(0, 0, 0))
     camera_moving = new Camera(-30, 14.5, 15, new THREE.Vector3(0, 14.5, 15))
 
     camera_persp.rotateZ(-130 * Math.PI / 180)
